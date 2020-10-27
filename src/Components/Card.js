8113d03a242b5c715f2f4ea1947794b03855c9ab
Paul-Antoine GAST <pagast.pro@gmail.com>
2020-10-27 09:44:16 +0100

    Implémentation de la requête URL

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,15 @@
-import React from "react"
+import React from "react";
 
-function Card() {
+export default function Card(){
+
+    const url = 'https://www.blockchain.com/ticker#';
+
+    // Créer une requête
+    let requete = new XMLHttpRequest(); // Créer un objet
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
     return (
         <div>
             <h1>Card</h1>
